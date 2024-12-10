d81f68a9a15571feac890c0e237c02fbe86a39a5
rahulphadke <gotorahul@gmail.com>
2024-12-10 07:33:11 +0530

    Added HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+package ta.sb01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb01Application {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+	}
+
+}
